0ce62074a87852092c734942d90f12bb721e87d3
Rocco Howard <rocco@hnh.digital>
2019-08-18 10:21:37 +0000

    Tooltip init fixes

--- js/jquery/Tooltip.js
+++ js/jquery/Tooltip.js
@@ -1,31 +1,44 @@
 
 // Convert all text elements.
 $(function() {
-    if (typeof Tooltip != 'undefined') {
-      $.frontendAssets.init();
-    }
+  if (typeof Tooltip != 'undefined') {
+    $('[title]:not(.init-tooltip):not(.no-tooltip)').addClass('init-tooltip');
+    $.frontendAssets.init();
+  }
 });
 
 // Register tooltip.
 $.frontendAssets.register('tooltip', function(event) {
   var element = $(event.currentTarget);
 
-  if (typeof element.data('data-text') == 'undefined') {
-    element.data('data-text', element.attr('title'));
+  if (typeof element.data('tooltip') != 'undefined') {
+    return;
+  }
+
+  if (typeof element.data('tooltip-title') == 'undefined') {
+    element.data('tooltip-title', element.attr('title'));
     element.attr('title', '');
   }
 
+  if (element.data('tooltip-title') == '') {
+    return;
+  }
+
+  if (typeof element.data('tooltip-placement') == 'undefined') {
+    element.data('tooltip-placement', 'right');
+  }
+
   var options = {
-    'title' => element.data('data-text')
+    'title': element.data('tooltip-title')
   };
 
   $.each(['placement', 'trigger', 'delay', 'html', 'closeOnClickOutside', 'boundariesElement', 'boundariesElement'], function(index, key) {
-    if (typeof element.data('data-' + key) != 'undefined') {
-        optioins[key] = element.data('data-' + key);
+    if (typeof element.data('tooltip-' + key) != 'undefined') {
+      options[key] = element.data('tooltip-' + key);
     }
   });
 
-  new Tooltip(element, options);
+  element.data('tooltip', new Tooltip(element, options))
 
   element.trigger('extension::tooltip::applied');
 });
